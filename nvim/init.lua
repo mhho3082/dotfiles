16e7e76675452174e8a5593834c8993a8054c6c8
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-02-27 20:00:44 +0800

    Add config for markdown

--- nvim/init.lua
+++ nvim/init.lua
@@ -132,7 +132,19 @@ require('packer').startup(function()
 
     -- Markdown
     use 'godlygeek/tabular'
-    use 'preservim/vim-markdown'
+    use{
+        'preservim/vim-markdown',
+        requires = 'godlygeek/tabular',
+        config = function()
+            -- disable header folding
+            vim.g.vim_markdown_folding_disabled = 1
+            -- disable conceal
+            vim.g.vim_markdown_conceal = 0
+            -- diable math tex conceal
+            vim.g.tex_conceal = ""
+            vim.g.vim_markdown_math = 1
+        end,
+    }
 
     -- Generate Markdown TOC
     use {
