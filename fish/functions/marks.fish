1c5b6b406f67e7dc2de3f93feb2d7be60417a5f3
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-02-28 18:24:08 +0800

    Rename variables

--- fish/functions/marks.fish
+++ fish/functions/marks.fish
@@ -3,13 +3,13 @@
 # Requires fzf
 
 # Location of bookmark file
-set --global bookmark_path "/home/$USER/.cd_bookmarks"
+set --global cd_bookmark_path "/home/$USER/.cd_bookmarks"
 
 # Add preview
 if which exa &>/dev/null
-    set --global preview_string "exa {} --all --long --header --icons --sort=ext --git"
+    set --global cd_bookmark_preview_string "exa {} --all --long --header --icons --sort=ext --git"
 else
-    set --global preview_string "ls {} -AlhF --group-directories-first"
+    set --global cd_bookmark_preview_string "ls {} -AlhF --group-directories-first"
 end
 
 # Add completion
@@ -21,59 +21,59 @@ complete -f -c marks -n "not __fish_seen_subcommand_from $bookmark_commands" -a 
 function marks --description "cd with bookmarks"
     if begin [ "$argv[1]" = a ]; or [ "$argv[1]" = "add" ]; end # Add pwd to bookmarks
         # Add only if not found
-        if ! grep -Fxq "$PWD" $bookmark_path
-            echo $PWD >>$bookmark_path
+        if ! grep -Fxq "$PWD" $cd_bookmark_path
+            echo $PWD >>$cd_bookmark_path
         end
 
         # Sort the bookmarks
-        sort -o $bookmark_path{,}
+        sort -o $cd_bookmark_path{,}
         return
     end
 
     if begin [ "$argv[1]" = e ]; or [ "$argv[1]" = "edit" ]; end # Edit bookmarks
         # Allow editing of bookmarks
-        $VISUAL $bookmark_path
+        $VISUAL $cd_bookmark_path
 
         # Expand ~
-        sed -i -e 's|^\~|/home/'"$USER"'|' $bookmark_path
+        sed -i -e 's|^\~|/home/'"$USER"'|' $cd_bookmark_path
 
         # Remove trailing /
-        sed -i -e 's|/$||' $bookmark_path
+        sed -i -e 's|/$||' $cd_bookmark_path
 
         # Remove duplicates
-        gawk -i inplace '!a[$0]++' $bookmark_path
+        gawk -i inplace '!a[$0]++' $cd_bookmark_path
 
         # Remove empty lines
-        sed -i '/^$/d' $bookmark_path
+        sed -i '/^$/d' $cd_bookmark_path
         
         # Sort the bookmarks
-        sort -o $bookmark_path{,}
+        sort -o $cd_bookmark_path{,}
         return
     end
 
     if begin [ "$argv[1]" = r ]; or [ "$argv[1]" = "remove" ]; end # Remove pwd from bookmarks
         # Find and remove pwd (except newline)
-        sed -i -e 's|^'"$PWD"'$||' $bookmark_path
+        sed -i -e 's|^'"$PWD"'$||' $cd_bookmark_path
 
         # Remove the newline
-        sed -i '/^$/d' $bookmark_path
+        sed -i '/^$/d' $cd_bookmark_path
         return
     end
 
     # The core function
     if which fzf &>/dev/null
         # Create file if not found
-        if test ! -f $bookmark_path
-            touch $bookmark_path
+        if test ! -f $cd_bookmark_path
+            touch $cd_bookmark_path
         end
 
-        set -l dest_dir ( cat $bookmark_path |
+        set -l dest_dir ( cat $cd_bookmark_path |
                           sed 's|^\~|/home/'"$USER"'|' | # Expand ~
                           sed 's|/$||' |                 # Remove trailing /
                           sed '/^\s*$/d' |               # Remove leading whitespace
                           sed '/^*\s$/d' |               # Remove trailing whitespace
                           sort | uniq |                  # Remove duplicates
-                          fzf --preview $preview_string  --prompt="marks> ")
+                          fzf --preview $cd_bookmark_preview_string  --prompt="marks> ")
 
         if [ "$dest_dir" != "" ]
             cd $dest_dir
