2da745f06c97255a1bf624df2feeec30625cafee
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-03-02 22:52:30 +0800

    Add encoding

--- nvim/init.lua
+++ nvim/init.lua
@@ -621,7 +621,7 @@ require('lualine').setup {
             { 'filename', path = 1, symbols = { modified = '' } },
             { 'diagnostics', sources = { 'nvim_lsp' } }
         },
-        lualine_x = { 'filetype', 'fileformat', '%3p%%' },
+        lualine_x = { 'filetype', 'fileformat', 'encoding', '%3p%%' },
         lualine_y = {},
         lualine_z = {},
     },
