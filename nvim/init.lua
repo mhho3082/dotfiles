223d9c2641e4a867f7bfa482c94a1c0778a2034f
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-03-05 23:06:31 +0800

    Use tab instead of enter for completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -570,12 +570,7 @@ cmp.setup {
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
         ["<Tab>"] = function(fallback)
             if require("luasnip").expand_or_jumpable() then
                 vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
