050833a32309a797c9decd6f510226e48d366128
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-03-13 20:22:41 +0800

    Add check for tmux (even if auto-start commented out)

--- fish/config.fish
+++ fish/config.fish
@@ -66,7 +66,9 @@ if status is-interactive
     # Commands to run in interactive sessions
 
     # # Auto-launch tmux (if tmux is closed, fish closes too)
-    # if not set -q TMUX
-    #     exec tmux
+    # if which tmux &>/dev/null
+    #     if not set -q TMUX
+    #         exec tmux
+    #     end
     # end
 end
