67fdf81cd55028ca968bfc052fde9afb087f0196
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-03-14 14:51:00 +0800

    I'm never gonna auto-start tmux
    I need to open terminal from file explorer, so no.

--- fish/config.fish
+++ fish/config.fish
@@ -64,11 +64,4 @@ end
 
 if status is-interactive
     # Commands to run in interactive sessions
-
-    # # Auto-launch tmux (if tmux is closed, fish closes too)
-    # if which tmux &>/dev/null
-    #     if not set -q TMUX
-    #         exec tmux
-    #     end
-    # end
 end
