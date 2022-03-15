3c6fc282563c1807dcfb206226a13994e9353fc8
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-03-16 01:42:03 +0800

    Use original tabline

--- nvim/init.lua
+++ nvim/init.lua
@@ -656,13 +656,6 @@ require('lualine').setup {
         lualine_y = {},
         lualine_z = {},
     },
-    tabline = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-    },
+    tabline = {},
     extensions = { 'nvim-tree', 'fugitive' },
 }
