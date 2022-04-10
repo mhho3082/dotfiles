9349ab703c59139146050ec7063eb56edf1293ed
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-04-10 20:42:02 +0800

    Use command instead of which

--- autostart/99-elecom-huge.sh
+++ autostart/99-elecom-huge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if which xinput &> /dev/null then
+if command -v xinput &> /dev/null then
 	# Set button Fn3 to scroll mode
 	# https://www.reddit.com/r/Trackballs/comments/drdp6f/elecom_huge_trackball_scroll_emulation/
 	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12
