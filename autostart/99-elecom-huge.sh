c6638f2f3f054856086a7282713d753e4be7e311
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-04-10 21:39:04 +0800

    Return to Fn3

--- autostart/99-elecom-huge.sh
+++ autostart/99-elecom-huge.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 if command -v xinput &> /dev/null; then
-	# Set button Fn3 to scroll mode
 	# https://www.reddit.com/r/Trackballs/comments/drdp6f/elecom_huge_trackball_scroll_emulation/
-	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12
-	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Scroll Method Enabled' 0 0 1
+	# https://www.reddit.com/r/Trackballs/comments/mtdgld/elecom_huge_button_configuration_ideas/
+
+	# Set Fn3 to scroll mode
+	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12 2> /dev/null
+	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Scroll Method Enabled' 0 0 1 2> /dev/null
 fi
