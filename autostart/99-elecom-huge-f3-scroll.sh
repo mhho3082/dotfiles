3e861061ef3b72e9f919d24b7d72e8123df69477
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-04-10 20:30:27 +0800

    Add autostart for elecom huge scroll with f3 button

---
+++ autostart/99-elecom-huge-f3-scroll.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Set button Fn3 to scroll mode
+# https://www.reddit.com/r/Trackballs/comments/drdp6f/elecom_huge_trackball_scroll_emulation/
+
+xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12
+xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Scroll Method Enabled' 0 0 1
