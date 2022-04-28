74f0143b7228a25773866b5979f4a086138276fe
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-04-28 17:07:21 +0800

    Add reminder to truly set auto-start

--- autostart/99-elecom-huge.sh
+++ autostart/99-elecom-huge.sh
@@ -4,6 +4,9 @@ if command -v xinput &> /dev/null; then
 	# https://www.reddit.com/r/Trackballs/comments/drdp6f/elecom_huge_trackball_scroll_emulation/
 	# https://www.reddit.com/r/Trackballs/comments/mtdgld/elecom_huge_button_configuration_ideas/
 
+	# Remember to set this to really auto-start with your desktop environment
+	# e.g., for xfce: set to "launch on login" in sessions and startup
+
 	# Set Fn3 to scroll mode
 	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12 2> /dev/null
 	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Scroll Method Enabled' 0 0 1 2> /dev/null
