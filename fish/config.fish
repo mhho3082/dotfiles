b85f58babf102d2e28e52d3b1161f26259d2c3ee
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-05-18 12:36:35 +0800

    FZF use ctrl-t-specific options

--- fish/config.fish
+++ fish/config.fish
@@ -58,8 +58,8 @@ end
 if which fzf &>/dev/null
     # Setup fzf with fd as default source
     set --global --export FZF_DEFAULT_COMMAND 'fd --type f -H -I -E "*.*.package" -E ".svn" -E ".git" -E ".hg" -E "node_modules" -E "bower_components"'
-    set --global --export FZF_DEFAULT_OPTS "--preview='less {}'"
     set --global --export FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+    set --global --export FZF_CTRL_T_OPTS "--preview='less {}'"
 
     # Initiate fzf key bindings
     # <C-t> - find files
