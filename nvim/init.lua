7978ef6c6160076cb1efafc9e084fac0b3901a49
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-06-03 11:11:42 +0800

    Add cmp cmdline mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -571,6 +571,7 @@ cmp.setup {
 
 -- Command line autocomplete
 cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
         { name = 'path' }
     }, {
