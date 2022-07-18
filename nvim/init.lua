14bb8383c5eb161d59e617fc021141c1f930b62d
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-07-19 01:15:03 +0800

    Add the code action icon back

--- nvim/init.lua
+++ nvim/init.lua
@@ -139,6 +139,7 @@ packer.startup(function()
 
       saga.init_lsp_saga({
         border_style = "rounded",
+        code_action_icon = " ",
         code_action_lightbulb = {
           enable = false,
         },
