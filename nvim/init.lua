173ccd1afbd5406e02d35cb259ecc1ad8b79b756
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-08-28 23:14:11 +0800

    Add `null-ls`

--- nvim/init.lua
+++ nvim/init.lua
@@ -143,6 +143,16 @@ packer.startup(function()
     end,
   })
   use("neovim/nvim-lspconfig")
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("null-ls").setup({
+        sources = {
+          require("null-ls").builtins.formatting.stylua,
+        },
+      })
+    end,
+  })
 
   -- LSP server status
   use({
