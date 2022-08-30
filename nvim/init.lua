72500a23cc1f8952899ab23b3bbff6ecd1eb41e2
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-08-30 18:28:46 +0800

    Use `prettierd`

--- nvim/init.lua
+++ nvim/init.lua
@@ -148,6 +148,7 @@ packer.startup(function()
       require("null-ls").setup({
         sources = {
           require("null-ls").builtins.formatting.stylua,
+          require("null-ls").builtins.formatting.prettierd,
         },
       })
     end,
