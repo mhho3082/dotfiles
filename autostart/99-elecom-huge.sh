5a9161650be76aa79384da633380ee6c1f96d131
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-10-25 16:54:45 +0800

    Enhance ELECOM huge config

--- autostart/99-elecom-huge.sh
+++ autostart/99-elecom-huge.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 if command -v xinput &> /dev/null; then
-	# https://www.reddit.com/r/Trackballs/comments/drdp6f/elecom_huge_trackball_scroll_emulation/
-	# https://www.reddit.com/r/Trackballs/comments/mtdgld/elecom_huge_button_configuration_ideas/
+    # https://www.reddit.com/r/Trackballs/comments/drdp6f/elecom_huge_trackball_scroll_emulation/
+    # https://www.reddit.com/r/Trackballs/comments/mtdgld/elecom_huge_button_configuration_ideas/
 
-	# Remember to set this to really auto-start with your desktop environment
-	# e.g., for xfce: set to "launch on login" in sessions and startup
+    # Remember to auto-start this with your desktop environment
+    # e.g., for xfce: set to "launch on login" in sessions and startup
+    
+    num=$(xinput list | grep "ELECOM TrackBall Mouse HUGE TrackBall" | \
+        grep "pointer" | grep -Eo "id=[0-9]+" | grep -Eo "[0-9]+")
 
-	# Set Fn3 to scroll mode
-	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Button Scrolling Button' 12 2> /dev/null
-	xinput set-prop "pointer:ELECOM TrackBall Mouse HUGE TrackBall" 'libinput Scroll Method Enabled' 0 0 1 2> /dev/null
+    # Set Fn3 to scroll mode
+    xinput set-prop $num 'libinput Button Scrolling Button' 12
+    xinput set-prop $num 'libinput Scroll Method Enabled' 0 0 1
+    xinput set-button-map $num 1 2 3 4 5 6 7 8 9 10 11 2
 fi
