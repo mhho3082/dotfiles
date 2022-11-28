428d1f50e78745ba2116407fb9a2fa941d9548ec
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-11-28 17:25:46 +0800

    FIX: no need to restart running services?

--- fish/functions/qemuprep.fish
+++ fish/functions/qemuprep.fish
@@ -1,5 +1,5 @@
 function qemuprep
-    sudo systemctl restart libvirtd
-    sudo systemctl restart firewalld
+    sudo systemctl enable libvirtd
+    sudo systemctl enable firewalld
     sudo virsh net-start default
 end
