1e585f59747bd55b3448640cc1483c12f8c18c99
Max Ho <73323597+mhho3082@users.noreply.github.com>
2022-12-04 17:19:02 +0800

    FEAT: Change qemuprep to virt
    Reflect that it now calls virt-manager too

--- fish/functions/qemuprep.fish
+++
@@ -1,9 +0,0 @@
-function qemuprep
-    sudo systemctl enable libvirtd
-    sudo systemctl enable firewalld
-    sudo virsh net-start default
-
-    if command -v virt-manager &>/dev/null
-        virt-manager
-    end
-end
