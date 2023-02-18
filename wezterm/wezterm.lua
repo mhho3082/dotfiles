a9ce5564e9e8247d6bdcc71dbfa0dc3cbcbc39d3
Max Ho <73323597+mhho3082@users.noreply.github.com>
2023-02-18 22:56:32 +0800

    fix: Format

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,19 +2,14 @@ local wezterm = require("wezterm")
 return {
   -- Use JetBrains Mono, which is built-in
   font = wezterm.font("JetBrains Mono"),
-
   -- Disable the tab bar
   enable_tab_bar = false,
-
   -- Use gruvbox
   color_scheme = "Gruvbox dark, hard (base16)",
-
   -- Disable default key bindings (e.g., for C-S-[left/right] for tmux)
   disable_default_key_bindings = true,
-
   -- Handle i3wm properly
   adjust_window_size_when_changing_font_size = false,
-
   keys = {
     -- Add back copy/paste
     {
