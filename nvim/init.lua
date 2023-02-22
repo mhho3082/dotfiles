94d99f42055d424500609bbbaa6b511492e27f2f
Max Ho <73323597+mhho3082@users.noreply.github.com>
2023-02-22 08:30:03 +0800

    fix: Add eslint to nvim null-ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -190,6 +190,7 @@ packer.startup(function()
       require("null-ls").setup({
         sources = {
           require("null-ls").builtins.diagnostics.zsh,
+          require("null-ls").builtins.diagnostics.eslint,
           require("null-ls").builtins.formatting.clang_format,
           require("null-ls").builtins.formatting.stylua,
           require("null-ls").builtins.formatting.prettierd.with({
