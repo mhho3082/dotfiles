65527d92adc222999bcf4b25b11ccdcaf569941c
Max Ho <73323597+mhho3082@users.noreply.github.com>
2023-03-09 18:40:06 +0800

    fix: Do not dim inactive "window"s

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,7 +108,6 @@ packer.startup(function()
       vim.background = "dark"
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_dim_inactive_windows = 1
       vim.cmd.colorscheme("gruvbox-material")
     end,
   })
