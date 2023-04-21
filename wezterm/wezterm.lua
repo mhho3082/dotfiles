e144af644b0e6a6de03a9a5ab299460986fc9da8
Max Ho <73323597+mhho3082@users.noreply.github.com>
2023-04-22 00:06:01 +0800

    fix: Use modernized wezterm config method

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,51 +1,67 @@
 local wezterm = require("wezterm")
-return {
-  -- Use JetBrains Mono, which is built-in
-  font = wezterm.font("JetBrains Mono"),
-  -- Disable the tab bar
-  enable_tab_bar = false,
-  -- Use gruvbox
-  color_scheme = "Gruvbox dark, hard (base16)",
-  -- Disable default key bindings (e.g., for C-S-[left/right] for tmux)
-  disable_default_key_bindings = true,
-  -- Handle i3wm properly
-  adjust_window_size_when_changing_font_size = false,
-  keys = {
-    -- Copy/paste
-    {
-      key = "c",
-      mods = "CTRL|SHIFT",
-      action = wezterm.action.CopyTo("Clipboard"),
-    },
-    {
-      key = "v",
-      mods = "CTRL|SHIFT",
-      action = wezterm.action.PasteFrom("Clipboard"),
-    },
-    -- Change font size
-    {
-      key = "-",
-      mods = "CTRL",
-      action = wezterm.action.DecreaseFontSize,
-    },
-    {
-      key = "=",
-      mods = "CTRL",
-      action = wezterm.action.IncreaseFontSize,
-    },
-    {
-      key = "0",
-      mods = "CTRL",
-      action = wezterm.action.ResetFontSize,
-    },
-    -- Scroll up/down
-    {
-      key = "PageUp",
-      action = wezterm.action.ScrollByPage(-0.5),
-    },
-    {
-      key = "PageDown",
-      action = wezterm.action.ScrollByPage(0.5),
-    },
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Use JetBrains Mono, which is built-in
+config.font = wezterm.font("JetBrains Mono")
+
+-- Disable the tab bar
+config.enable_tab_bar = false
+
+-- Use gruvbox
+config.color_scheme = "Gruvbox dark, hard (base16)"
+
+-- Use non-blinking bar by default
+config.default_cursor_style = "SteadyBar"
+
+-- Disable default key bindings (e.g., for C-S-[left/right] for tmux)
+config.disable_default_key_bindings = true
+
+-- Handle i3wm properly
+config.adjust_window_size_when_changing_font_size = false
+
+config.keys = {
+  -- Copy/paste
+  {
+    key = "c",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.CopyTo("Clipboard"),
+  },
+  {
+    key = "v",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.PasteFrom("Clipboard"),
+  },
+  -- Change font size
+  {
+    key = "-",
+    mods = "CTRL",
+    action = wezterm.action.DecreaseFontSize,
+  },
+  {
+    key = "=",
+    mods = "CTRL",
+    action = wezterm.action.IncreaseFontSize,
+  },
+  {
+    key = "0",
+    mods = "CTRL",
+    action = wezterm.action.ResetFontSize,
+  },
+  -- Scroll up/down
+  {
+    key = "PageUp",
+    action = wezterm.action.ScrollByPage(-0.5),
+  },
+  {
+    key = "PageDown",
+    action = wezterm.action.ScrollByPage(0.5),
   },
 }
+
+return config
