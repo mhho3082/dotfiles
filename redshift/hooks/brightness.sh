008876bbfca43f2c6f809026c0fe8eaafcf65782
Max Ho <73323597+mhho3082@users.noreply.github.com>
2023-04-29 15:29:12 +0800

    fix: Use redshift to control screen brightness

---
+++ redshift/hooks/brightness.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Set brightness via xbrightness when redshift status changes
+
+# Deeply based on https://wiki.archlinux.org/title/Redshift
+# Main change is that brightnessctl is used instead
+
+# Set brightness values for each status.
+# Range from 0% to 100%, and 0 to 255, are valid
+brightness_day='70%'
+brightness_transition='50%'
+brightness_night='30%'
+# Adjust this grep to filter only the backlights you want to adjust
+backlights=($(brightnessctl --list | grep "'backlight'" | sed "s/.*'\([^']*\)'.*/\1/"))
+
+set_brightness() {
+    for backlight in "${backlights[@]}"
+    do
+        brightnessctl set $1 &
+    done
+}
+
+if [ "$1" = period-changed ]; then
+    case $3 in
+        night)
+            set_brightness $brightness_night
+            ;;
+        transition)
+            set_brightness $brightness_transition
+            ;;
+        daytime)
+            set_brightness $brightness_day
+            ;;
+    esac
+fi
