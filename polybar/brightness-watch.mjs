1fbe32182f30d1da0509799f4484280cc4d2a45f
Max Ho <73323597+mhho3082@users.noreply.github.com>
2024-01-26 02:26:50 +0800

    fix: Use tailing script for brightness on polybar

---
+++ polybar/brightness-watch.mjs
@@ -0,0 +1,27 @@
+import fs from 'fs/promises';
+import { exec } from 'child_process';
+
+const backlights = await fs.readdir("/sys/class/backlight");
+if (backlights.length === 0) {
+  throw new Error("No backlight devices found!");
+}
+
+const file_path = `/sys/class/backlight/${backlights[0]}/brightness`;
+
+const output = () => {
+  exec(
+    "brightnessctl",
+    (_err, stdout, _stderr) => {
+      const percentage = stdout.trim().match(/(\d+)%/)?.[1];
+      const icon = percentage >= 70 ? "󰃠" : percentage >= 30 ? "󰃟" : "󰃞";
+      console.log(`%{F#83A598}${icon}%{F-} ${percentage}%`);
+    }
+  );
+}
+
+// Run once first
+output();
+
+// Watch the brightness file
+const watcher = fs.watch(file_path);
+for await (const _ of watcher) { output(); }
