a4aa28b6568cc997e3e9bfc067770dccfc0cc27a
Max Ho <73323597+mhho3082@users.noreply.github.com>
2024-08-14 15:19:54 +0800

    fix: detect .env.* files as conf filetype

--- nvim/init.lua
+++ nvim/init.lua
@@ -919,12 +919,18 @@ vim.opt.spelllang = "en_gb"
 
 -- Markdown code block rendering
 vim.g.markdown_minlines = 100
--- Add alias for files (for markdown inline syntax highlight)
+
+-- Add alias for files
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/2131
 -- https://github.com/neovim/neovim/pull/16600
 vim.filetype.add({
   extension = {
+    -- For markdown inline block
     matplotlib = "python",
+  },
+  pattern = {
+    -- https://neovim.discourse.group/t/how-to-add-custom-filetype-detection-to-various-env-files/4272
+    [".env.*"] = "config",
   },
 })
 
