a306aecb0c0f2a2fb1ede51523ef00a2252317c2
Max Ho <73323597+mhho3082@users.noreply.github.com>
2026-05-14 14:11:00 +0800

    fix: harper use british grammar, andd stop asking to turn TODO into to-do

--- nvim/init.lua
+++ nvim/init.lua
@@ -779,6 +779,23 @@ vim.schedule(function()
     keymap("n", "<M-" .. ops .. ">", "<C-w><" .. ops .. ">")
   end, { "Left", "Down", "Up", "Right" })
 
+  -- incremental selection treesitter/LSP
+  -- Based on https://github.com/pawelgrzybek/dotfiles/blob/master/nvim/lua/keymaps.lua#L92-L107
+  keymap({ "n", "x", "o" }, "<M-o>", function()
+    if vim.treesitter.get_parser(nil, nil, { error = false }) then
+      require("vim.treesitter._select").select_parent(vim.v.count1)
+    else
+      vim.lsp.buf.selection_range(vim.v.count1)
+    end
+  end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
+  keymap({ "n", "x", "o" }, "<M-i>", function()
+    if vim.treesitter.get_parser(nil, nil, { error = false }) then
+      require("vim.treesitter._select").select_child(vim.v.count1)
+    else
+      vim.lsp.buf.selection_range(-vim.v.count1)
+    end
+  end, { desc = "Select child treesitter node or inner incremental lsp selections" })
+
   -- Allow zz to work in visual mode (for the whole selection)
   local function center_visual_selection()
     vim.cmd([[ execute "normal! \<ESC>" ]]) -- Force exit from visual mode
@@ -973,7 +990,7 @@ vim.schedule(function()
   })
 
   require("mason").setup({})
-  require("mason-lspconfig").setup({ ensure_installed = { "copilot" } })
+  require("mason-lspconfig").setup({})
 
   local _, blink = pcall(require, "blink.cmp")
   if blink then
@@ -1012,7 +1029,12 @@ vim.schedule(function()
     },
   })
   vim.lsp.config("harper_ls", {
-    settings = { ["harper-ls"] = { linters = { SentenceCapitalization = false, SpellCheck = false } } },
+    settings = {
+      ["harper-ls"] = {
+        dialect = "British",
+        linters = { ToDoHyphen = false, SentenceCapitalization = false, SpellCheck = false },
+      },
+    },
   })
   vim.lsp.config("jdtls", { settings = { java = { format = { enabled = false } } } })
   -- https://github.com/LazyVim/LazyVim/discussions/2159
